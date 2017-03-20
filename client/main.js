dd005c61d95a77ef74e1bc583aef53719a8fc4d0
luiz.quelves <luiz.quelves@gmail.com>
2017-03-20 17:38:23 -0300

    add files

---
+++ client/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
+Meteor.startup(() => {
+	render(<App />, document.getElementById('render-target'));
+});
+
+class App extends Component {
+	render() {
+		return (
+			<h1>rtk03</h1>
+			);
+	}
+}
+
